5ccdf4b75bf275a6b4d760d1bb12a7f6a1892772
Jergsen <jorgen.tal@gmail.com>
2024-08-24 03:00:08 +0200

    yespls

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('_http_common');
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
